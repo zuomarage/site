7fdbedfa0e7fc0d5cd3c3e3b4611acb5b448d8e0
Julie Kim Amouzegar <90331904+zuomarage@users.noreply.github.com>
2025-01-02 08:51:10 +0100

    assets

---
+++ assets/theme-editor.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+
+	document.addEventListener('shopify:section:load', function (event) {
+		const section = event.target;
+
+		if (typeof CartDrawer !== 'undefined') {
+			new CartDrawer();
+		}
+		if (section.classList.contains('product-section')) {
+			window.dispatchEvent(new Event('resize'));
+		}
+	});
+});
